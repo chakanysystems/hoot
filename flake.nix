1b0bf9f74ea6e6c0c022d5f789bfb3d32bf06f31
Jack Chakany <jack@chakany.systems>
2025-09-03 20:10:46 -0400

    Update Nix Flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    nixGL.url = "github:nix-community/nixGL";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
