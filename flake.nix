8dbceac549ca046e5d0752e70c939f9a3a80bead
Jack Chakany <jack@chakany.systems>
2026-02-07 18:21:18 -0500

    half working flake until we do something about egui_tabs

--- flake.nix
+++ flake.nix
@@ -1,55 +1,201 @@
 {
-  description = "Hoot Email Client";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    nixGL.url = "github:nix-community/nixGL";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
-        basePkgs = with pkgs; [
-          (
-            rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" "rust-analyzer" ];
-            }
-          )
-          libiconv
-          pkg-config
-          fontconfig
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      devenv,
+      rust-overlay,
+      flake-parts,
+      nixgl,
+      crane,
+      advisory-db,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{ config, moduleWithSystem, ... }:
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
         ];
 
-        linuxPkgs = with pkgs; [
-          wayland
-          libxkbcommon
-          libGL
-        ];
+        perSystem =
+          { config, system, ... }:
+          let
+            overlays = [
+              (import rust-overlay)
+              nixgl.overlay
+            ];
+            pkgs = import nixpkgs {
+              inherit system overlays;
+            };
 
-        darwinPkgs = with pkgs; [
-          apple-sdk
-        ];
+            rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+              ];
+            };
 
-        allPkgs = basePkgs ++ (if pkgs.stdenv.isDarwin then darwinPkgs else []) ++ (if pkgs.stdenv.isLinux then linuxPkgs else []);
-      in
-        {
-          devShells.default = with pkgs; mkShell {
-            buildInputs = allPkgs;
+            craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
-            LD_LIBRARY_PATH =
-              builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" allPkgs;
+            rustPlatform = pkgs.makeRustPlatform {
+              cargo = rustToolchain;
+              rustc = rustToolchain;
+            };
 
-            shellHook = ''
-              echo "Welcome to the Hoot Devshell. You should be good to go!"
+            src = craneLib.cleanCargoSource ./.;
+
+            commonArgs = {
+              inherit src;
+              strictDeps = true;
+
+              nativeBuildInputs = with pkgs; [
+                pkg-config
+                perl
+                makeWrapper
+              ];
+
+              buildInputs =
+                with pkgs;
+                [
+                  fontconfig
+                ]
+                ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+                  wayland
+                  libxkbcommon
+                  libGL
+                  xorg.libX11
+                  xorg.libXcursor
+                  xorg.libXrandr
+                  xorg.libXi
+                ]
+                ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+                  darwin.apple_sdk.frameworks.AppKit
+                  darwin.apple_sdk.frameworks.CoreGraphics
+                  darwin.apple_sdk.frameworks.Foundation
+                ];
+            };
+
+            # Use rustPlatform for building to avoid crane's git dependency issues
+            hoot = craneLib.buildPackage {
+              inherit src;
+              inherit (commonArgs) nativeBuildInputs buildInputs;
+
+              postInstall = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+                wrapProgram $out/bin/hoot \
+                  --prefix LD_LIBRARY_PATH : "${
+                    pkgs.lib.makeLibraryPath [
+                      pkgs.wayland
+                      pkgs.libxkbcommon
+                      pkgs.libGL
+                      pkgs.xorg.libX11
+                      pkgs.xorg.libXcursor
+                      pkgs.xorg.libXrandr
+                      pkgs.xorg.libXi
+                    ]
+                  }"
+              '';
+            };
+
+            # Keep crane artifacts for checks only
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            nixgl-wrapper = pkgs.writeShellScriptBin "hoot-nixgl" ''
+              exec ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${hoot}/bin/hoot "$@"
             '';
+          in
+          {
+            checks = {
+              inherit hoot;
+
+              hoot-clippy = craneLib.cargoClippy (
+                commonArgs
+                // {
+                  inherit cargoArtifacts;
+                  cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+                }
+              );
+
+              hoot-doc = craneLib.cargoDoc (
+                commonArgs
+                // {
+                  inherit cargoArtifacts;
+                  env.RUSTDOCFLAGS = "--deny warnings";
+                }
+              );
+
+              hoot-fmt = craneLib.cargoFmt {
+                inherit src;
+              };
+
+              hoot-toml-fmt = craneLib.taploFmt {
+                src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
+              };
+
+              hoot-audit = craneLib.cargoAudit {
+                inherit src advisory-db;
+              };
+
+              hoot-deny = craneLib.cargoDeny {
+                inherit src;
+              };
+            };
+            packages = rec {
+              devenv-up = config.devShells.default.config.procfileScript;
+              devenv-test = config.devShells.default.config.test;
+              default = nixgl-wrapper;
+              hoot-unwrapped = hoot;
+            };
+
+            formatter = pkgs.nixfmt-tree;
+
+            apps.default = {
+              type = "app";
+              program = "${nixgl-wrapper}/bin/hoot-nixgl";
+            };
+
+            devShells.default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                (
+                  { pkgs, ... }:
+                  {
+                    packages = [
+                      rustToolchain
+                    ];
+                  }
+                )
+              ];
+            };
           };
-        }
+      }
     );
 }
