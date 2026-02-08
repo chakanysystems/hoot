6250a9512e957bb36de183c1b8ab1995a4bb33ab
Jack Chakany <jack@chakany.systems>
2026-02-08 16:20:55 -0500

    Make unlock page look nicer

--- src/ui/unlock_database.rs
+++ src/ui/unlock_database.rs
@@ -1,4 +1,5 @@
 use crate::{Hoot, HootStatus};
+use eframe::egui;
 use eframe::egui::Ui;
 use tracing::error;
 
@@ -9,52 +10,84 @@ pub struct UnlockDatabaseState {
 }
 
 #[derive(Debug)]
-pub struct UnlockDatabase {}
+pub struct UnlockDatabase;
 
 impl UnlockDatabase {
     pub fn ui(app: &mut Hoot, ui: &mut Ui) {
-        ui.vertical(|ui| {
-            ui.heading("Unlock Hoot");
-            ui.label("If this is your first time using Hoot, then the password you enter will be your encryption password.");
-            ui.text_edit_singleline(&mut app.state.unlock_database.secret_input);
-            ui.horizontal(|ui| {
-                if ui.button("Unlock").clicked() {
-                    match app
-                        .db
-                        .unlock_with_password(app.state.unlock_database.secret_input.clone())
+        egui::Frame::none()
+            .inner_margin(egui::Margin::same(20.0))
+            .show(ui, |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.add_space(80.0);
+
+                    ui.heading(egui::RichText::new("Unlock Hoot").size(24.0));
+                    ui.add_space(10.0);
+
+                    ui.label(
+                        egui::RichText::new("Enter your password to unlock the database")
+                            .color(ui.visuals().weak_text_color()),
+                    );
+                    ui.add_space(30.0);
+
+                    if !app.state.unlock_database.error_string.is_empty() {
+                        ui.colored_label(
+                            egui::Color32::RED,
+                            format!("⚠ {}", app.state.unlock_database.error_string),
+                        );
+                        ui.add_space(10.0);
+                    }
+
+                    ui.label("Password:");
+                    ui.add_sized(
+                        [300.0, 24.0],
+                        egui::TextEdit::singleline(&mut app.state.unlock_database.secret_input)
+                            .password(true)
+                            .hint_text("Enter password"),
+                    );
+                    ui.add_space(20.0);
+
+                    if ui
+                        .add_sized(
+                            [200.0, 40.0],
+                            egui::Button::new(egui::RichText::new("Unlock").size(15.0)),
+                        )
+                        .clicked()
                     {
-                        Ok(v) => {
-                            app.state.unlock_database.secret_input = String::new(); // clear input
-                            app.status = HootStatus::Initalizing;
-                            app.page = crate::Page::Inbox;
-                        }
-                        Err(e) => {
-                            let mut unknown_error = || {
-                                app.state.unlock_database.error_string = e.to_string();
-                            };
-                            error!("Error when trying to load database: {}", e);
-                            app.state.unlock_database.secret_input = String::new();
-                            match e.downcast_ref::<rusqlite_migration::Error>() {
-                                Some(rusqlite_migration::Error::RusqliteError { query, err }) => {
-                                    match err.sqlite_error_code() {
-                                        Some(rusqlite::ErrorCode::NotADatabase) => {
-                                            // So, here this case would likely mean that the unlock didn't go so well.
-                                            // But, it is "NotADatabase", so it could also mean that it's... not a database. aka corruption?
-                                            error!("Wrong password given or the database is corrupted.");
-                                            app.state.unlock_database.error_string = "Wrong password".into();
-                                        }
-                                        Some(_) => unknown_error(),
-                                        None => unknown_error()
-                                    }
-                                }
-                                Some(_) => unknown_error(),
-                                None => unknown_error()                            }
+                        Self::attempt_unlock(app);
+                    }
+                });
+            });
+    }
+
+    fn attempt_unlock(app: &mut Hoot) {
+        match app
+            .db
+            .unlock_with_password(app.state.unlock_database.secret_input.clone())
+        {
+            Ok(_) => {
+                app.state.unlock_database.secret_input.clear();
+                app.state.unlock_database.error_string.clear();
+                app.status = HootStatus::Initalizing;
+                app.page = crate::Page::Inbox;
+            }
+            Err(e) => {
+                error!("Error when trying to load database: {}", e);
+                app.state.unlock_database.secret_input.clear();
+
+                let msg = match e.downcast_ref::<rusqlite_migration::Error>() {
+                    Some(rusqlite_migration::Error::RusqliteError { err, .. }) => {
+                        match err.sqlite_error_code() {
+                            Some(rusqlite::ErrorCode::NotADatabase) => {
+                                error!("Wrong password given or the database is corrupted.");
+                                "Wrong password".to_string()
+                            }
+                            _ => e.to_string(),
                         }
                     }
-                }
-
-                ui.label(&app.state.unlock_database.error_string);
-            });
-        });
+                    _ => e.to_string(),
+                };
+                app.state.unlock_database.error_string = msg;
+            }
+        }
     }
 }
